50dd0efb8474bf14d4b6dfef536774ca7e1e0b82
Christopher Tjondro <christopherbrian88@gmail.com>
2025-04-14 16:41:05 -0700

    Updated the scripts for classes and objects, formatted answers and comments better

--- scripts/02-object-oriented-ruby/classes_and_objects.rb
+++ scripts/02-object-oriented-ruby/classes_and_objects.rb
@@ -1,10 +1,11 @@
+# > Classes and Objects I
 # Create a class called MyCar. 
 # - When you initialize a new instance or object of the class, allow the user to define some instance variables that tell us the year, color, and model of the car. 
 # - Create an instance variable that is set to 0 during instantiation of the object to track the current speed of the car as well. 
 # - Create instance methods that allow the car to speed up, brake, and shut the car off.
 
-# - Add an accessor method to your MyCar class to change and view the color of your car. 
-# - Then add an accessor method that allows you to view, but not modify, the year of your car.
+# Add an accessor method to your MyCar class to change and view the color of your car. 
+# Then add an accessor method that allows you to view, but not modify, the year of your car.
 
 
 # You want to create a nice interface that allows you to accurately describe the action you want your program to perform. 
@@ -15,6 +16,10 @@
 # instance methods: speed_up, brake, shut_engine
 # accessors: view & change car color
 # readers: year
+
+# > Classes and Objects II
+# Add a class method to your MyCar class that calculates the gas mileage (i.e. miles per gallon) of any car.
+# Override the to_s method to create a user friendly print out of your object.
 
 module SprayPaint
   def spray_paint(new_color)
--- scripts/02-object-oriented-ruby/classes_and_objects_extraction_refactor.rb
+++ scripts/02-object-oriented-ruby/classes_and_objects_extraction_refactor.rb
@@ -1,10 +1,11 @@
+# > Classes and Objects I
 # Create a class called MyCar. 
 # - When you initialize a new instance or object of the class, allow the user to define some instance variables that tell us the year, color, and model of the car. 
 # - Create an instance variable that is set to 0 during instantiation of the object to track the current speed of the car as well. 
 # - Create instance methods that allow the car to speed up, brake, and shut the car off.
 
-# - Add an accessor method to your MyCar class to change and view the color of your car. 
-# - Then add an accessor method that allows you to view, but not modify, the year of your car.
+# Add an accessor method to your MyCar class to change and view the color of your car. 
+# Then add an accessor method that allows you to view, but not modify, the year of your car.
 
 
 # You want to create a nice interface that allows you to accurately describe the action you want your program to perform. 
@@ -16,6 +17,22 @@
 # accessors: view & change car color
 # readers: year
 
+# > Classes and Objects II
+# Add a class method to your MyCar class that calculates the gas mileage (i.e. miles per gallon) of any car.
+# Override the to_s method to create a user friendly print out of your object.
+
+# > Inheritance
+# Create a superclass called Vehicle for your MyCar class to inherit from and move the behavior that isn't specific to the MyCar class to the superclass. 
+# - Create a constant in your MyCar class that stores information about the vehicle that makes it different from other types of Vehicles.
+
+# Then create a new class called MyTruck that inherits from your superclass that also has a constant defined that separates it from the MyCar class in some way.
+# Add a class variable to your superclass that can keep track of the number of objects created that inherit from the superclass. Create a method to print out the value of this class variable as well.
+# Create a module that you can mix in to ONE of your subclasses that describes a behavior unique to that subclass.
+# Print to the screen your method lookup for the classes that you have created. (my_class.ancestors)
+# > Move all of the methods from the MyCar class that also pertain to the MyTruck class into the Vehicle class. Make sure that all of your previous method calls are working when you are finished.
+# > Write a method called age that calls a private method to calculate the age of the vehicle. Make sure the private method is not available from outside of the class. You'll need to use Ruby's built-in Time class to help.
+
+
 module SprayPaint
   def spray_paint(new_color)
     self.color = new_color
@@ -24,7 +41,7 @@ end
 
 module Towable
   def can_tow?(pounds)
-    pounds < 2000
+    pounds < 2000 # All Towables apparently can only tow up to 2000 lbs
   end
 end
 
@@ -137,3 +154,39 @@ end
   
 # line 107 is a no method error since @name the instance variable is read-only and has no getter defined via attr_reader.
 # Change to attr_writer or attr_accessor to fix.
+
+# Inheritance Q7
+# puts "Well done!" if joe.better_grade_than?(bob)
+
+# ANS:
+# class Student
+#   def initialize(name, grade)
+#     @name = name
+#     @grade = grade
+#   end
+
+#   def better_grade_than?(other_student)
+#     grade > other_student.grade
+#   end
+
+#   protected
+
+#   def grade
+#     @grade
+#   end
+# end
+
+# joe = Student.new("Joe", 90)
+# bob = Student.new("Bob", 84)
+# puts "Well done!" if joe.better_grade_than?(bob)
+
+# Inheritance Q8
+# bob = Person.new
+# bob.hi
+
+# ERROR:
+# NoMethodError: private method `hi' called for #<Person:0x007ff61dbb79f0>
+# from (irb):8
+# from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+# FIX: move the hi method outside (i.e. above) the private method call in the class.
